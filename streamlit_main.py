02fba477f51756981d38710c85d3036648ce60da
PRAGADEESH-1207 <158313384+PRAGADEESH-1207@users.noreply.github.com>
2024-05-05 18:12:21 +0530

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -41,7 +41,7 @@ app_mode = st.sidebar.selectbox("Select Page", ["Home", "About Project", "Predic
 if app_mode == "Home":
     st.header("FRUITS & VEGETABLES RECOGNITION SYSTEM")
     image_path = "vf/thomas-le-pRJhn4MbsMM-unsplash.jpg"
-    st.image.open(image_path)
+    image.open(image_path)
 
 # About Project
 elif app_mode == "About Project":
